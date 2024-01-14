292cf0cbb81fe6bfdd268bde75a3d5ce3faaa7cf
Sumit05092002 <129786205+Sumit05092002@users.noreply.github.com>
2024-01-14 17:14:54 +0530

    home page almost complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import Image from 'next/image'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="logo">
+        <Image src="/rcc 1.png" height={80} width={90} alt="logo" className='logo' ></Image>
+      </div>
+      <nav>
+        <button className='login'>Login</button>
+        <button className='signup'>SignUp</button>
+      </nav>
+    </div>
   )
 }
 
