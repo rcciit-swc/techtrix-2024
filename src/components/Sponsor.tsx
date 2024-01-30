0e8db5327e79dd15ab0ea9656b4025429660a1ee
Debopom-Banerjee <debopombanerjee752@gmail.com>
2024-01-30 12:51:33 +0530

    add:sponsors.tsx setup to be done.

--- src/components/Sponsor.tsx
+++ src/components/Sponsor.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import Image from "next/image";
+import Heading from "./Heading";
 const Sponsor = () => {
   return (
-    <div>Sponsor</div>
-  )
-}
+    <div className="w-[80%] mx-auto my-10 gap-6  flex-col flex justify-center items-center">
+      <Heading text="Sponsors" />
+      <div className="w-[80%] mx-auto my-10 gap-6  flex-col flex justify-center items-center ">
+        <Image
+          src="/assets/sponsors/frame.svg"
+          width={0}
+          height={0}
+          className="w-[50%] bottom-[-90%]  items-center absolute cursor-pointer hover:border-yellow-400 duration-500 hover:scale-105"
+          alt="frame"
+        />
+      </div>
+      <div>
+      <Image
+          src="/assets/sponsors/gadget.svg"
+          width={0}
+          height={0}
+          className="w-[15%] bottom-[-50%] right-[23%]  items-center absolute cursor-pointer"
+          alt="frame"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Sponsor
+export default Sponsor;
