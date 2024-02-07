95336b70567186b869cefc16aa49a01fa55877fe
soumyarajbag <soumyarajbag@gmail.com>
2024-02-07 07:29:22 +0530

    changes:supabase

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,12 +4,11 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <div className="w-[100%] mx-auto relative  flex flex-row  justify-start max-lg:mt-[50px]">
-       {/* <div className="flex  flex-row items-center -rotate-90  left-0 absolute bottom-0">
+      {/* <div className="flex  flex-row items-center -rotate-90  left-0 absolute bottom-0">
             <Image src={"/Line 1.svg"} alt="" width={0} height={0} className="" />
             <h1 className="text-lg  left-0">RCC INSTITUTE OF INFORMATION TECHNOLOGY</h1>
         </div> */}
       <div className="w-[80%] md:w-[75%] mx-auto relative">
-     
         <img
           src={"/Rectangle 1.png"}
           className="w-full"
