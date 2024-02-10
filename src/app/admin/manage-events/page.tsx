8d94e80c855d340e4923eb91eb2cea8b597e8334
soumyarajbag <soumyarajbag@gmail.com>
2024-02-10 14:30:35 +0530

    add:manage-events

--- src/app/admin/manage-events/page.tsx
+++ src/app/admin/manage-events/page.tsx
@@ -1,321 +1,86 @@
-"use client";
+"use client"
 import { Heading } from "@/components/home";
-import { coordinatorType, eventInputType } from "@/types/events";
-import { addEvent } from "@/utils/functions";
-import React, { useEffect, useState } from "react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
-const FormElement = ({
-  name,
-  value,
-  type,
-  id,
-  onChange,
-}: {
-  name: string;
-  value: string;
-  type: string;
-  id: string;
-  onChange: (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => void;
-}) => {
-  return (
-    <div className="flex flex-row  items-center gap-1 md:gap-5 flex-wrap justify-start">
-      <label htmlFor={id} className="font-semibold text-base md:text-xl">
-        {name} :
-      </label>
-      <input
-        type={type}
-        value={value}
-        onChange={onChange}
-        name={id}
-        id={id}
-        className="border-black px-2 py-1 rounded-xl "
-      />
-    </div>
-  );
-};
-
+import React from "react";
+import { IoSearchSharp } from "react-icons/io5";
+import { FaPlus } from "react-icons/fa6";
+import Image from "next/image";
+import Link from "next/link";
 const page = () => {
-  const [inputs, setInputs] = useState<eventInputType>({
-    name: "",
-    description: "",
-    category: "",
-    date: "",
-    time: "",
-    minTeamSize: 1,
-    maxTeamSize: 1,
-    venue: "",
-    coordinators: [],
-    price: "",
-    prize: "",
-    rules: "",
-    imagePath: "",
-  });
-  console.log(inputs);
-  const handleInputChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | any>
-  ) => {
-    const { name, value } = e.target;
-    setInputs((prevInputs) => ({
-      ...prevInputs,
-      [name]: value,
-    }));
-  };
-
-  const handleQuillChange = (value: string, name: string) => {
-    setInputs((prevInputs) => ({
-      ...prevInputs,
-      [name]: value,
-    }));
-  };
-
-  const handleAddCoordinator = (coordinator: coordinatorType) => {
-    setInputs((prevInputs) => ({
-      ...prevInputs,
-      coordinators: [...prevInputs.coordinators, coordinator],
-    }));
-  };
-  const handleRemoveCoordinator = (index: number) => {
-    setInputs((prevInputs) => ({
-      ...prevInputs,
-      coordinators: prevInputs.coordinators.filter((_, idx) => idx !== index),
-    }));
-  };
-  const [isCoordinatorFormOpen, setIsCoordinatorFormOpen] = useState(false);
-  const openCoordinatorForm = () => {
-    setIsCoordinatorFormOpen(true);
-  };
-
-  const closeCoordinatorForm = () => {
-    setIsCoordinatorFormOpen(false);
-  };
+  const [isOpen, setIsOpen] = React.useState(false);
+  const onClose = () => setIsOpen(false);
   return (
-    <div className="flex flex-col items-center justify-center gap-5 w-[80%] mx-auto overflow-x-hidden">
+    <div className="flex flex-col items-center mx-auto gap-5">
       <Heading text="Manage Events" />
-      <div className="mx-auto border-2 border-black rounded-xl bg-gray-100 flex flex-col  flex-wrap gap-10 w-full px-2 py-5  md:px-10 md:py-10">
-        <div className=" flex flex-row items-center gap-8 md:gap-20 flex-wrap justify-start w-full  ">
-          <FormElement
-            name="Category"
-            value={inputs.category}
-            id="category"
-            onChange={(e: any) => handleInputChange(e)}
-            type="text"
-          />
-          <FormElement
-            name="Event Name"
-            value={inputs.name}
-            id="name"
-            onChange={(e: any) => handleInputChange(e)}
-            type="text"
-          />
-          <FormElement
-            name="Date"
-            value={inputs.date}
-            id="date"
-            onChange={(e: any) => handleInputChange(e)}
-            type="date"
-          />
-          <FormElement
-            name="Time"
-            value={inputs.time}
-            id="time"
-            onChange={(e: any) => handleInputChange(e)}
-            type="time"
-          />
-          <FormElement
-            name="Registration Fees"
-            value={inputs.price}
-            id="price"
-            onChange={(e: any) => handleInputChange(e)}
-            type="text"
-          />
-          <FormElement
-            name="Prize Pool"
-            value={inputs.prize}
-            id="prize"
-            onChange={(e: any) => handleInputChange(e)}
-            type="text"
-          />
-          <FormElement
-            name="Min Team Size"
-            value={inputs.minTeamSize.toString()}
-            id="minTeamSize"
-            onChange={(e: any) => handleInputChange(e)}
-            type="number"
-          />
-          <FormElement
-            name="Max Team Size"
-            value={inputs.maxTeamSize.toString()}
-            id="maxTeamSize"
-            onChange={(e: any) => handleInputChange(e)}
-            type="number"
-          />
-          <FormElement
-            name="Venue"
-            value={inputs.venue}
-            id="venue"
-            onChange={(e: any) => handleInputChange(e)}
-            type="text"
-          />
-          <FormElement
-            name="Image Path"
-            value={inputs.imagePath}
-            id="imagePath"
-            onChange={(e: any) => handleInputChange(e)}
-            type="text"
-          />
-        </div>
-        <div className="w-full flex flex-row max-md:flex-wrap items-center gap-5">
-          <div className="md:w-1/2 flex flex-col gap-5">
-            <div className="flex flex-row  items-center gap-1 w-full md:gap-5 flex-wrap justify-start">
-              <label
-                htmlFor={"description"}
-                className="font-semibold md:text-xl"
-              >
-                Description :
-              </label>
-              <ReactQuill
-                theme="snow"
-                value={inputs.description}
-                onChange={(value) => handleQuillChange(value, "description")}
-              />
-            </div>
-
-            <div className="flex flex-row  items-center gap-1 md:gap-5 flex-wrap justify-start">
-              <label
-                htmlFor={"description"}
-                className="font-semibold md:text-xl"
-              >
-                Rules :
-              </label>
-              <ReactQuill
-                theme="snow"
-                value={inputs.rules}
-                onChange={(value) => handleQuillChange(value, "rules")}
-              />
-            </div>
-          </div>
-          <div className="md:w-1/2 text-center flex flex-col gap-3">
-            {inputs.coordinators.length == 0 ? (
-              <h1>No Coordinators Added yet !</h1>
-            ) : (
-              <div>
-                <h2 className="font-semibold text-xl ">Coordinators</h2>
-                <ul className="flex flex-col items-center gap-2">
-                  {inputs.coordinators.map((coordinator, index) => (
-                    <li
-                      key={index}
-                      className="border-2 border-black rounded-xl px-2 py-1"
-                    >
-                      <p className="text-black font-semibold text-lg">
-                        {index + 1}. {coordinator.name}
-                      </p>
-                      <p className="text-black font-semibold text-lg">
-                        {coordinator.phone}
-                      </p>
-                      <button
-                        onClick={() => handleRemoveCoordinator(index)}
-                        className="text-red-500 border-red-500 border-2 mt-3 rounded-full px-2  font-semibold"
-                      >
-                        Remove
-                      </button>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-            <button
-              onClick={openCoordinatorForm}
-              className="font-semibold md:w-1/2 mx-auto text-sm md:text-lg border-2 border-black rounded-full  px-2  py-1"
-            >
-              ADD COORDINATOR
-            </button>
-          </div>
-        </div>
-        <button
-          onClick={() => addEvent(inputs)}
-          className="text-xl border-2 border-black w-1/3 mx-auto rounded-full px-2 py-1 text-black"
-        >
-          Submit
+      <div className="flex  md:flex-row w-[90%]  gap-2 mx-auto items-center justify-center flex-wrap">
+        <input
+          type="text"
+          placeholder="Search for Events"
+          className="w-[70%] border-2 border-black rounded-full py-4 px-4"
+        />
+        <IoSearchSharp className="w-10 h-10" />
+        <button  className="flex flex-row items-center font-semibold border-2 border-black px-5 py-2 hover:bg-white hover:text-black rounded-xl bg-black text-white gap-2 text-xl ">
+          <FaPlus />
+          <Link href={"/admin/manage-events/add-event"}>Add Event</Link>
         </button>
-      </div>
 
-      <CoordinatorForm
-        isOpen={isCoordinatorFormOpen}
-        onClose={closeCoordinatorForm}
-        onAddCoordinator={handleAddCoordinator}
-      />
+        <div className="border-2 border-black rounded-xl flex flex-row flex-wrap-reverse items-start px-5 py-5 gap-10 justify-between w-[80%]">
+      <div className="flex flex-col items-start gap-5 font-semibold">
+        <h1 className="text-3xl  tracking-wider">Event Name</h1>
+        <h1>Category: AUTOMATA</h1>
+        <div className="flex flex-row items-center gap-2 w-full justify-between flex-wrap">
+          <h1>Date: 12th March, 2024</h1>
+          <h1>Time: 12 PM</h1>
+        </div>
+        <div className="flex flex-row items-center gap-2 w-full justify-between flex-wrap">
+          <h1>Fees: 400 /-</h1>
+          <h1>Prize : 2000 /-</h1>
+        </div>
+        <h1>Venue: Baranagar</h1>
+
+        <p>
+          Description: <br />
+          <span className="font-medium">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Perferendis minima optio repellendus eaque, totam reiciendis
+            quisquam voluptatem adipisci maiores nemo aliquid enim provident at
+            laudantium excepturi, eos asperiores, quaerat inventore sapiente
+            aspernatur.
+          </span>
+        </p>
+        <button className="border-2 border-black px-5 py-1 rounded-full bg-black text-white hover:bg-white hover:text-black">
+          View Rules
+        </button>
+        {/* <h1>Coordinators:</h1>
+        <div className='flex flex-col items-center gap-2 -mt-3 flex-semibold'>
+        <h1>John Doe : +91 8337045160</h1>
+        <h1>Jane Doe : +91 8337045160</h1>
+        </div> */}
+      </div>
+      <img src={"/valorant.jpg"} alt="" className="w-80" height={0} width={0} />
+    </div>
+      </div>
+      {isOpen && <RulesModal isOpen={isOpen} onClose={onClose} />}
     </div>
   );
 };
 
-const CoordinatorForm = ({
+
+const RulesModal = ({
   isOpen,
   onClose,
-  onAddCoordinator,
+  
 }: {
   isOpen: boolean;
   onClose: () => void;
-  onAddCoordinator: (coordinator: coordinatorType) => void;
 }) => {
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState("");
-  const handleSubmit = () => {
-    if (name && phone) {
-      onAddCoordinator({ name, phone,email });
-      setName("");
-      setPhone("");
-      onClose();
-    }
-  };
-
- 
+   
   return (
     <>
       {isOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-gray-100 p-4 rounded-lg">
             <h2 className="text-lg font-semibold mb-2">Add Coordinator</h2>
-            <div className="flex flex-col gap-4">
-              <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Name"
-                className="border-black px-2 py-1 rounded-xl"
-              />
-                <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Email"
-                className="border-black px-2 py-1 rounded-xl"
-              />
-              <input
-                type="text"
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-                placeholder="Phone"
-                className="border-black  px-2 py-1 rounded-xl"
-              />
-              <div className="flex justify-end">
-                <button
-                  onClick={onClose}
-                  className="bg-gray-300 px-4 py-2 rounded-md mr-2"
-                >
-                  Cancel
-                </button>
-                <button
-                  onClick={handleSubmit}
-                  className="bg-blue-500 text-white px-4 py-2 rounded-md"
-                >
-                  Add
-                </button>
-              </div>
-            </div>
+            
           </div>
         </div>
       )}
