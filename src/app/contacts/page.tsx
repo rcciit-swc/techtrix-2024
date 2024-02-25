c43013990a4a48163c2110ac0ddc3a21128186ef
Arpan Ghosh <arpanghosh5115@gmail.com>
2024-02-26 02:23:39 +0530

    add: contacts

---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,16 @@
+import Block from '@/components/contacts/Block'
+import Committee from '@/components/contacts/Committee'
+import Location from '@/components/contacts/Location'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Committee />
+      <Location/>
+      <Block/>
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/contacts/Committee.tsx
@@ -0,0 +1,43 @@
+import { CommitteeDetails } from "@/utils/constants/committee";
+
+const Committee = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-8 lg:gap-16 ">
+      {CommitteeDetails.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="flex flex-col items-center gap-3 px-5 lg:gap-5"
+          >
+            <h1 className="text-center font-got text-lg tracking-wider text-primary lg:text-2xl">
+              {item.title}
+            </h1>
+            <div className="flex flex-row flex-wrap items-center justify-center gap-8 lg:gap-20">
+              {item.members.map((member, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="text-md flex flex-row flex-wrap items-center gap-2 lg:text-xl"
+                  >
+                    <div className="flex flex-col items-center text-center">
+                      <p className="font-semibold">{member.name}</p>
+                      <p>{member.role}</p>
+                      <a
+                        href={`tel:${member.phone}`}
+                        className="hover:text-green-400"
+                      >
+                        {member.phone}
+                      </a>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Committee;
