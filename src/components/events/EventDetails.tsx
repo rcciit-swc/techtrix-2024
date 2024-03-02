fbaaa74d7a7b40dec89b48f5c56665ab33e68c73
soumyarajbag <soumyarajbag@gmail.com>
2024-03-02 06:02:48 +0530

    fix

--- src/components/events/EventDetails.tsx
+++ src/components/events/EventDetails.tsx
@@ -71,9 +71,7 @@ const EventDetails = ({
               <h1>
                 Team Capacity :{" "}
                 <span className="font-semibold">
-                  {eventInfo.min_team_member}{" "}
-                  {eventInfo.max_team_member > 1 &&
-                    "to " + eventInfo.max_team_member}
+                  {eventInfo.min_team_member == eventInfo.max_team_member ? eventInfo.max_team_member  : eventInfo.min_team_member + " to " + eventInfo.max_team_member}
                 </span>
               </h1>
               <h1>
