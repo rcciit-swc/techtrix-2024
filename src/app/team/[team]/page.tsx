14d32d2623deae35c59d621d62b7c27561e9d1a7
soumyarajbag <soumyarajbag@gmail.com>
2024-03-23 00:00:21 +0530

    small fixes

--- src/app/team/[team]/page.tsx
+++ src/app/team/[team]/page.tsx
@@ -11,7 +11,7 @@ const page = ({ params: { team } }: { params: { team: string } }) => {
   return (
     <div className="flex flex-col items-center gap-10 oveflow-x-hidden min-h-[60vh]">
       <div className="flex mt-5 flex-col items-center  gap-5 justify-center">
-        <h1 className="text-3xl font-bold">{teamData!.category}</h1>
+        <h1 className="text-3xl font-bold text-center">{teamData!.category}</h1>
         <div className="flex flex-row gap-10 md:gap-32 mt-5 flex-wrap justify-center">
           {teamData!.members.map((member: any, index: number) => {
             return <TeamCard member={member} key={index} />;
